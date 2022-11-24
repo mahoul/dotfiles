de9046ba8d6ba0c6de64f449291ef318d579a121
Enrique Gil <mahoul@gmail.com>
2022-11-24 22:37:18 +0100

    Restored i3-gnome config

--- i3-gnome/bin/gnome-settings-tweaks.sh
+++ i3-gnome/bin/gnome-settings-tweaks.sh
@@ -1,20 +1,33 @@
 #!/bin/bash
 
-gsettings set org.gnome.gnome-flashback desktop false
-gsettings set org.gnome.gnome-flashback notifications false
-gsettings set org.gnome.gnome-flashback root-background false
+
+# Disable desktop management for notificacions on gnome-flashback 
+# and disable background drawing too.
+gsettings set org.gnome.gnome-flashback desktop 		false
+gsettings set org.gnome.gnome-flashback notifications 		false
+gsettings set org.gnome.gnome-flashback root-background 	false
 gsettings set org.gnome.gnome-flashback status-notifier-watcher false
 
-gsettings set org.gnome.Terminal.Legacy.Settings headerbar false
-gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar false
-gsettings set org.gnome.Terminal.Legacy.Settings menu-accelerator-enabled false
-gsettings set org.gnome.Terminal.Legacy.Settings theme-variant 'dark'
+# Tune-up the gnome-terminal
+# 
+gsettings set org.gnome.Terminal.Legacy.Settings headerbar 			false
+gsettings set org.gnome.Terminal.Legacy.Settings default-show-menubar 		false
+gsettings set org.gnome.Terminal.Legacy.Settings menu-accelerator-enabled 	false
+gsettings set org.gnome.Terminal.Legacy.Settings theme-variant 			'dark'
 
-gsettings set org.gnome.desktop.interface gtk-theme 'Nord-GTK'
-gsettings set org.gnome.desktop.interface icon-theme 'Gruvbox-Material-Dark'
-gsettings set org.gnome.desktop.interface document-font-name 'Fira Code Medium 11'
-gsettings set org.gnome.desktop.interface font-name 'Fira Sans Medium 11'
-gsettings set org.gnome.desktop.interface monospace-font-name 'Fira Code Medium 10'
+# Adjust the themes on Gnome
+gsettings set org.gnome.desktop.interface gtk-theme 	'Nordic-bluish-accent-v40'
+gsettings set org.gnome.desktop.interface icon-theme 	'Papirus-Dark'
 
+# Adjust the fonts
+#
+gsettings set org.gnome.desktop.interface document-font-name 	'Fira Sans Medium 11'
+gsettings set org.gnome.desktop.interface font-name 		'Fira Sans Medium 11'
+gsettings set org.gnome.desktop.interface monospace-font-name 	'JetBrains Mono Medium 11'
+
+# Enable night light 
 gsettings set org.gnome.settings-daemon.plugins.color night-light-enabled true
+
+# Restore gnome-terminal profiles
+dconf load /org/gnome/terminal/legacy/profiles:/ < ~/.config/gnome-terminal-profiles
 
--- screenlayout/.screenlayout/one-monitor.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-xrandr --output DVI-D-0 --off --output HDMI-0 --off --output DP-0 --primary --mode 1920x1080 --pos 0x0 --rotate left --output DP-1 --off
-sleep 5s
-gnome-extensions disable gTile@vibou
-gnome-extensions enable gTile@vibou
