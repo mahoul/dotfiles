ffadaa9d3ffbabcbf13abfae039899807196fe5a
Enrique Gil <mahoul@gmail.com>
2022-11-29 19:38:40 +0100

    Tweak set-scale.sh script

--- i3-gnome/bin/set-scale.sh
+++ i3-gnome/bin/set-scale.sh
@@ -4,18 +4,17 @@ SF=$(gsettings get org.gnome.desktop.interface text-scaling-factor)
 
 if [ "$SF" != "1.25" ]; then
 	gsettings set org.gnome.desktop.interface text-scaling-factor 1.25
+	if pidof polybar &>/dev/null; echo $?; then
+		sed -i 's/dpi =.*/dpi = 125/g' ~/.config/polybar/config
+		i3-msg restart
+	fi
 else
 	gsettings set org.gnome.desktop.interface text-scaling-factor 1.00
+	if pidof polybar &>/dev/null; echo $?; then
+		sed -i 's/dpi =.*/dpi = 100/g' ~/.config/polybar/config
+		i3-msg restart
+	fi
 fi
 
-if pidof polybar &>/dev/null; echo $?; then
-	POLY_SF=$(grep dpi ~/.config/polybar/config | awk '{ print $3 }')
-	if [ "$POLY_SF" != 125 ]; then
-		sed -i 's/dpi =.*/dpi = 125/g' ~/.config/polybar/config
-	else
-                sed -i 's/dpi =.*/dpi = 100/g' ~/.config/polybar/config
-	fi
-	i3-msg restart
-fi
 
 
